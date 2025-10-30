c921e5042a12f91501762f8936d43e95a1422fd8
Ronald Tan <ronald.tan.hong@student.mmu.edu.my>
2025-10-30 21:11:29 +0800

    Bank_Transfer_Page

--- Bank_Transfer_Page.php
+++ Bank_Transfer_Page.php
@@ -3,7 +3,7 @@
 date_default_timezone_set("Asia/Kuala_Lumpur");
 
 // ✅ 连接数据库
-$conn = new mysqli("localhost", "root", "", "donation system");
+$conn = new mysqli("localhost", "root", "", "fyp_test");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
