6064a6f88fc3af6135fc34f1337daa8e1c023d0a
Ronald Tan <ronald.tan.hong@student.mmu.edu.my>
2025-11-15 13:12:04 +0800

    Activity Page

--- Activity Page.php
+++ Activity Page.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "donation system");
+$conn = new mysqli("localhost", "root", "", "donation_system");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
